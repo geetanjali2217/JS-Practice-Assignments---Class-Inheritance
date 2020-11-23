6602deddbf3ebf1e045e92bd7d0a73a4d93b3c54
Geetanjali Katare <68340329+geetanjali2217@users.noreply.github.com>
2020-11-23 13:43:26 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,5 @@
-// import using require
-
-// declare class
-
-
 // export class using module.exports
+const Shape = require("./shape.js");
+module.exports = class Circle extends Shape {
+  calculateArea() {}
+};
