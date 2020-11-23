f58d5fd99e08f60646de5413fb65bd827f91e9e1
Geetanjali Katare <68340329+geetanjali2217@users.noreply.github.com>
2020-11-23 13:44:02 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,10 @@
 // declare class
 
 // export class using module.exports
+module.exports = class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {}
+  calculateArea() {}
+};
